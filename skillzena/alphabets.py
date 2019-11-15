a61de2a1248443860d6b0c2cebbf818f700d9204
Shivay Lamba <shivaylamba@gmail.com>
2019-11-15 17:28:03 +0530

    Update alphabets.py

--- skillzena/alphabets.py
+++ skillzena/alphabets.py
@@ -1,1 +1,9 @@
-
+t=int(input())
+while(t):
+ s,z=int(input()),input()
+ m,c,a,t=1,1,z[0],t-1
+ for i in range(1,s):
+  c=c+1if z[i]==z[i-1]else 1
+  if c==m:a+=z[i]
+  (m,a)=(c,z[i])if c>m else(m,a)
+ print(m,''.join(sorted(set(a))))
